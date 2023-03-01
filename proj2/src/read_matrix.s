7c87ae5cda7ddd73cce512202aaf86c9d1f5ab43
wknclizu <wknclizu@gmail.com>
2023-03-01 12:12:37 +0800

    proj2 & lab5

--- proj2/src/read_matrix.s
+++ proj2/src/read_matrix.s
@@ -25,18 +25,168 @@
 #     this function terminates the program with error code 29
 # ==============================================================================
 read_matrix:
-
 	# Prologue
+	add t0, a0, x0
+	add t1, a1, x0
+	add t2, a2, x0
 
+	#fopen
+	addi sp, sp, -16
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw t2, 8(sp)
+	sw ra, 12(sp)
 
+	addi a1, x0, 0
+	jal ra fopen
+	blt a0, x0, exit_fopen
 
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw t2, 8(sp)
+	lw ra, 12(sp)
+	addi sp, sp, 16
 
+	# fread rows
+	add t0, a0, x0
+	add a1, t1, x0
+	addi a2, x0, 4
+	
+	addi sp, sp, -16
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw t2, 8(sp)
+	sw ra, 12(sp)
 
+	jal ra fread
+	addi t0, x0, 4
+    ebreak
+	bne a0, t0, exit_fread
 
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw t2, 8(sp)
+	lw ra, 12(sp)
+	addi sp, sp, 16
+	lw t1, 0(t1)
+	# end of fread
+	# t1 stores the value of rows
 
+	# fread columns
+	add a0, t0, x0
+	add a1, t2, x0
+	addi a2, x0, 4
+	addi sp, sp, -16
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw t2, 8(sp)
+	sw ra, 12(sp)
 
+	jal ra fread
+	addi t0, x0, 4
+	bne a0, t0, exit_fread
+
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw t2, 8(sp)
+	lw ra, 12(sp)
+	addi sp, sp, 16
+	lw t2, 0(t2)
+	# end of fead
+	# t2 stores the value of columns
+    ebreak
+
+	mul t3, t1, t2
+	slli t3, t3, 2
+	addi sp, sp, -16
+	sw s0, 0(sp)
+	sw s1, 4(sp)
+	sw s2, 8(sp)
+	sw s3, 12(sp)
+	add s0, t0, x0 # s0: the file descriptor 
+	add s1, t1, x0 # s1: the value of rows
+	add s2, t2, x0 # s2: the value of cols
+
+	addi sp, sp, -4
+	sw ra, 0(sp)
+	add a0, t3, x0
+	jal ra, malloc
+	beq a0, x0, exit_malloc
+	add s3, a0, x0 # s3: the beginning address of the array
+	lw ra, 0(sp)
+	addi sp, sp, 4
+
+	addi t1, x0, 0
+	addi t3, x0, 0
+
+loop_1:
+	bge t1, s1, loop_1_end
+	addi t2, x0, 0
+
+loop_2:
+	beq t2, s2, loop_2_end
+
+	addi sp, sp, -16
+	sw t1, 0(sp)
+	sw t2, 4(sp)
+	sw t3, 8(sp)
+	sw ra, 12(sp)
+
+	add a0, s0, x0
+	add a1, s3, t3
+	addi a2, x0, 4
+	jal ra, fread
+	addi t0, x0, 4
+	bne a0, t0, exit_fread
+
+	lw t1, 0(sp)
+	lw t2, 4(sp)
+	lw t3, 8(sp)
+	lw ra, 12(sp)
+	addi sp, sp, 16
+	addi t3, t3, 4
+
+	addi t2, t2, 1
+	j loop_2
+
+loop_2_end:
+
+	addi t1, t1, 1
+	j loop_1
+	
+loop_1_end:
 
 	# Epilogue
+	addi sp, sp, -4
+	sw ra, 0(sp)
+	addi a0, s0, 0
+	jal ra, fclose
+	bne a0, x0, exit_fclose
+	lw ra, 0(sp)
+	addi sp, sp, 4
 
+	addi a0, s3, 0
+
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw s2, 8(sp)
+	lw s3, 12(sp)
+	addi sp, sp, 16
 
 	ret
+
+exit_malloc:
+	li a0, 26
+	j exit
+
+exit_fopen:
+	li a0, 27
+	j exit
+
+exit_fclose:
+	li a0, 28
+	j exit
+
+exit_fread:
+	li a0, 29
+	j exit
--- proj2/src/write_matrix.s
+++ proj2/src/write_matrix.s
@@ -25,16 +25,125 @@
 write_matrix:
 
 	# Prologue
+	add t0, a0, x0
+	add t1, a1, x0
+	add t2, a2, x0
+	add t3, a3, x0
+	addi sp, sp, -8
+	sw t2, 0(sp)
+	sw t3, 4(sp)
+	addi t2, sp, 0
+	addi t3, sp, 4
 
+	#fopen
+	addi sp, sp, -20
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw t2, 8(sp)
+	sw ra, 12(sp)
+	sw t3, 16(sp)
 
+	addi a1, x0, 1
+	jal ra fopen
+	blt a0, x0, exit_fopen
 
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw t2, 8(sp)
+	lw ra, 12(sp)
+	lw t3, 16(sp)
+	addi sp, sp, 20
 
+	# fwrite rows
+	add t0, a0, x0
+	add a1, t2, x0
+	addi a2, x0, 1
+	addi a3, x0, 4
+	
+	addi sp, sp, -20
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw t2, 8(sp)
+	sw ra, 12(sp)
+	sw t3, 16(sp)
 
+	jal ra fwrite
+	addi t0, x0, 1
+	bne a0, t0, exit_fwrite
 
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw t2, 8(sp)
+	lw ra, 12(sp)
+	lw t3, 16(sp)
+	addi sp, sp, 20
+	# end of fwrite
 
+	# fwrite columns
+	add a0, t0, x0
+	add a1, t3, x0
+	addi a2, x0, 1
+	addi a3, x0, 4
+	
+	addi sp, sp, -20
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw t2, 8(sp)
+	sw ra, 12(sp)
+	sw t3, 16(sp)
 
+	jal ra fwrite
+	addi t0, x0, 1
+	bne a0, t0, exit_fwrite
 
-	# Epilogue
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw t2, 8(sp)
+	lw ra, 12(sp)
+	lw t3, 16(sp)
+	addi sp, sp, 20
+	# end of fwrite
 
+	lw t3, 0(t3s)
+	lw t2, 0(t2)
+	addi sp, sp, 8
+	
+	addi a0, t0, 0
+	addi a1, t1, 0
+	mul a2, t3, t2
+	addi a3, x0, 4
+	add t1, a2, x0
+	ebreak
+
+	addi sp, sp, -12
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw ra, 8(sp)
+	jal ra, fwrite
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw ra, 8(sp)
+	addi sp, sp, 12
+	bne t1, a0, exit_fwrite
+
+	addi sp, sp, -4
+	sw ra, 0(sp)
+	addi a0, t0, 0
+	jal ra, fclose
+	bne a0, x0, exit_fclose
+	lw ra, 0(sp)
+	addi sp, sp, 4
 
 	ret
+
+exit_fopen:
+	li a0, 27
+	j exit
+
+exit_fclose:
+	li a0, 28
+	j exit
+
+exit_fwrite:
+	li a0, 30
+	j exit
